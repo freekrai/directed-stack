e7bdcfb9a07037e411f398f6c52fe48e30c5caf8
Roger Stringer <roger@datamcfly.com>
2023-02-05 10:34:16 -0800

    Update md.server.ts
    Signed-off-by: Roger Stringer <roger@datamcfly.com>

--- app/utils/md.server.ts
+++ app/utils/md.server.ts
@@ -1,5 +1,5 @@
 import { parse, transform, type Config } from "@markdoc/markdoc";
-import { Callout, CodeBlock } from "~/components/markdown";
+import { Callout, Step, CodeBlock } from "~/components/markdown";
 
 export function parseMarkdown(markdown: string, options: Config = {}) {
 	return transform( parse(markdown), {
@@ -7,7 +7,8 @@ export function parseMarkdown(markdown: string, options: Config = {}) {
 			fence: CodeBlock.scheme
 		},
 		tags: { 
-			callout: Callout.scheme 
+			callout: Callout.scheme,
+			step: Step.scheme
 		},
 	});
 }
